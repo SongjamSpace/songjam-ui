f6875c5db0fb34f4e81da35aeb35c133cc42f3c1
logesh2496 <logesh.r24@gmail.com>
2025-07-25 17:15:52 +0530

    fix: demo lb

---
+++ src/services/db/demoLeaderboard.service.ts
@@ -0,0 +1,22 @@
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  limit,
+  query,
+} from 'firebase/firestore';
+import { db } from '../firebase.service';
+
+export const getDemoLeaderboard = async (queryId: string) => {
+  const docs = await getDocs(
+    // query(collection(db, 'demo', queryId, 'leaderboard'), limit(10))
+    collection(db, 'demo', queryId, 'leaderboard')
+  );
+  return docs.docs.map((doc) => doc.data());
+};
+
+export const queryIdExists = async (queryId: string) => {
+  const ss = await getDoc(doc(db, 'demo', queryId));
+  return ss.exists();
+};
